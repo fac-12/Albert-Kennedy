06772078dcd8165411bf6eaed9416d5515ba754a
Lucy King <lucy.m.king92@gmail.com>
2018-02-05 16:35:10 +0000

    Update button.js

--- client/src/components/button.js
+++ client/src/components/button.js
@@ -1,5 +1,9 @@
-export default Button = (props) => {
-  return <button type="submit" className="button" onClick="" style = {{ backgroundColor: props.color }}>
+import React from 'react';
+
+const Button = (props) => {
+  return <button type="submit" className="button" style = {{ backgroundColor: props.color }}>
   {props.text}
   </button>;
 };
+
+export default Button;
