b9669c883076c15b31eddfb448afaa25964e3e98
Lucy King <lucy.m.king92@gmail.com>
2018-02-12 15:13:11 +0000

    Update errorReducer.js

--- client/src/reducers/errorReducer.js
+++ client/src/reducers/errorReducer.js
@@ -3,7 +3,6 @@ import { DISPLAY_ERROR } from '../actions/types'
 export default (state = '', action) => {
   switch (action.type) {
     case DISPLAY_ERROR:
-    	console.log(action.payload);
       return action.payload;
     default:
       return state;
