d0a35bc746c9c7888e28d649d8d37cc8033c7b3f
hannahcmtucker <hannahcmtucker@gmail.com>
2018-02-13 20:35:12 +0000

    Add new appointment button

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -3,12 +3,14 @@ import Header from "../Header";
 import { Link } from "react-router-dom";
 import { fetchAppointments } from "../../actions/appointment";
 import { connect } from "react-redux";
+import LinkButton from "../LinkButton";
 
 class Profile extends Component {
   render() {
     return (
       <div>
         <Header heading="My Appointments" logout />
+        <LinkButton text="new appointment" url="/topics" primary />
         <p>
           Immediate crisis? Don't use this site -{" "}
           <Link to="/crisis">use these resources instead</Link>
