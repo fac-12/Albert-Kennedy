bd739f206eaf1e35f342b679e19a37878ca376b1
sophielevens <sophielevens1@gmail.com>
2018-02-15 21:12:12 +0000

    responsive design and link edit

--- client/src/components/LinkButton.js
+++ client/src/components/LinkButton.js
@@ -3,21 +3,21 @@ import history from "../history";
 import styled from "styled-components";
 
 const Button = styled.button`
-  background: ${props => (props.primary ? "#f47a20" : "white")};
-  border: 2px solid #f47a20;
-  border-radius: 5px;
-  width: 100vw;
-  height: 60px;
-  font-size: 16px;
-  margin: 5px;
+	background: ${props => (props.primary ? "#f47a20" : "white")};
+	border: 2px solid #f47a20;
+	border-radius: 5px;
+	width: 100vw;
+	height: 60px;
+	font-size: 16px;
+	margin: 5px;
 `;
 
 const LinkButton = props => {
-  return (
-    <Button onClick={() => history.push(props.url)} {...props}>
-      {props.text}
-    </Button>
-  );
+	return (
+		<Button onClick={() => history.push(props.url)} {...props}>
+			{props.text}
+		</Button>
+	);
 };
 
 export default LinkButton;
--- client/src/components/schedule/Topic.js
+++ client/src/components/schedule/Topic.js
@@ -15,130 +15,132 @@ import skills from "../../assets/skills.png";
 import wellbeing from "../../assets/wellbeing.png";
 
 const Input = styled.input`
-  display: none;
+	display: none;
 
-  &:checked + div {
-    color: #f47a20;
-  }
+	&:checked + div {
+		color: #f47a20;
+	}
 `;
 
 const Card = styled.label`
-  width: 160px;
-  height: 110px;
-  border-radius: 10px;
-  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.25);
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-  margin: 5px;
-  border-left: solid 8px #f47a20;
+	width: 160px;
+	height: 110px;
+	border-radius: 10px;
+	box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.25);
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	text-align: center;
+	margin: 5px;
+	border-left: solid 8px #f47a20;
 `;
 
 const Label = styled.div`
-  font-size: 0.9rem;
-  padding: 5px;
+	font-size: 0.9rem;
+	padding: 5px;
 `;
 
 const Img = styled.img`
-  height: 40px;
-  width: auto;
-  padding: 5px;
+	height: 40px;
+	width: auto;
+	padding: 5px;
 `;
 
 const FlexWrapper = styled.div`
-  width: 100vw;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  align-items: center;
+	width: 100vw;
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: center;
+	align-items: center;
 `;
 
 const Error = styled.div`
-  font-size: 0.8em;
-  font-weight: 500;
-  align-self: flex-start;
+	font-size: 0.8em;
+	font-weight: 500;
+	align-self: flex-start;
 `;
 
 class TopicForm extends Component {
-  render() {
-    const { handleSubmit } = this.props;
-    return (
-      <div className="container__div">
-        <Header heading="Let’s talk" text="Choose as many as apply" />
-        <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
-          <FlexWrapper>
-            <Field
-              name="housing"
-              label="Housing and homelessness"
-              src={housing}
-              component={this.renderField}
-            />
-            <Field
-              name="wellbeing"
-              label="Wellbeing"
-              src={wellbeing}
-              component={this.renderField}
-            />
-            <Field
-              name="comingout"
-              label="Coming out to family and friends"
-              src={comingout}
-              component={this.renderField}
-            />
-            <Field
-              name="bullying"
-              label="Bullying and abuse"
-              src={bullying}
-              component={this.renderField}
-            />
-            <Field
-              name="skills"
-              label="Skills"
-              src={skills}
-              component={this.renderField}
-            />
-            <Field
-              name="other"
-              label="Anything else!"
-              src={other}
-              component={this.renderField}
-            />
-          </FlexWrapper>
-          <SubmitButton text="next" />
-        </form>
-      </div>
-    );
-  }
+	render() {
+		const { handleSubmit } = this.props;
+		return (
+			<div className="container__div">
+				<Header heading="Let’s talk" text="Choose any that apply" />
+				<form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+					<FlexWrapper>
+						<Field
+							name="housing"
+							label="Housing and homelessness"
+							src={housing}
+							component={this.renderField}
+						/>
+						<Field
+							name="wellbeing"
+							label="Wellbeing"
+							src={wellbeing}
+							component={this.renderField}
+						/>
+						<Field
+							name="comingout"
+							label="Coming out to family and friends"
+							src={comingout}
+							component={this.renderField}
+						/>
+						<Field
+							name="bullying"
+							label="Bullying and abuse"
+							src={bullying}
+							component={this.renderField}
+						/>
+						<Field
+							name="skills"
+							label="Skills"
+							src={skills}
+							component={this.renderField}
+						/>
+						<Field
+							name="other"
+							label="Anything else!"
+							src={other}
+							component={this.renderField}
+						/>
+					</FlexWrapper>
+					<SubmitButton text="next" />
+				</form>
+			</div>
+		);
+	}
 
-  renderField = field => {
-    const { meta: { error, submitFailed } } = field;
-    return [
-      <Card htmlFor={field.name} key={1}>
-        <Img src={field.src} />
-        <Input id={field.name} type="checkbox" {...field.input} />
-        <Label>{field.label}</Label>
-      </Card>,
-      <Error className="error" key={2}>{submitFailed ? error : ""}</Error>
-    ];
-  };
+	renderField = field => {
+		const { meta: { error, submitFailed } } = field;
+		return [
+			<Card htmlFor={field.name} key={1}>
+				<Img src={field.src} />
+				<Input id={field.name} type="checkbox" {...field.input} />
+				<Label>{field.label}</Label>
+			</Card>,
+			<Error className="error" key={2}>
+				{submitFailed ? error : ""}
+			</Error>
+		];
+	};
 
-  onSubmit = values => {
-    this.props.updateTopics(values);
-  };
+	onSubmit = values => {
+		this.props.updateTopics(values);
+	};
 }
 
 const validate = values => {
-  const errors = {};
-  if (_.isEmpty(values)) {
-    errors.other =
-      "Please pick at least one topic, choose 'Anything else' if you are unsure.";
-  }
-  return errors;
+	const errors = {};
+	if (_.isEmpty(values)) {
+		errors.other =
+			"Please pick at least one topic, choose 'Anything else' if you are unsure.";
+	}
+	return errors;
 };
 
 export default reduxForm({
-  validate,
-  form: "TopicForm"
+	validate,
+	form: "TopicForm"
 })(connect(null, { updateTopics })(TopicForm));
