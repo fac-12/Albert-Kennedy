c8800e6d6fcd41e2f862d3bb78220c7802e1fc4b
sophielevens <sophielevens1@gmail.com>
2018-04-27 12:48:12 +0100

    resolves merge conflicts

--- client/src/components/landing/Landing.js
+++ client/src/components/landing/Landing.js
@@ -1,128 +1,19 @@
 import React, { Component } from "react";
 import backgroundImage from "../../assets/images/raw/landing-background.jpg";
 
-<<<<<<< HEAD
-import {Container,
-Mask,
-DesktopBg,
-Card,
-Img,
-LogIn,
-StyledLink,
-Subtitle,
-DesktopButton,
-H2} from "../styling/onboardingpages";
-
-=======
-const Container = styled.div`
-  @media (min-width: 630px) {
-    position: relative;
-    background-color: #7c53a2;
-  }
-`;
-
-const Mask = styled.div`
-  @media (min-width: 630px) {
-    opacity: 0.5;
-    background-color: #7c53a2;
-    width: 100vw;
-  }
-`;
-
-const DesktopBg = styled.div`
-  font-size: 1.2rem;
-  text-align: center;
-  display: flex;
-  flex-wrap: wrap;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-  @media (min-width: 630px) {
-    background-image: url(${backgroundImage});
-    background-repeat: no-repeat;
-    height: 100vh;
-    margin: 1rem 4rem;
-    background-size: 250%;
-    background-position: 50% 50%;
-    box-shadow: inset 0 1px 3px 0 rgba(0, 0, 0, 0.5);
-    background-color: #7c53a2;
-  }
-`;
-const Card = styled.div`
-  background: white;
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  z-index: 1;
-  margin-top: 2rem;
-  @media (min-width: 630px) {
-    position: absolute;
-    width: 25rem;
-    height: 25rem;
-    padding: 1rem;
-    z-index: 2;
-  }
-`;
-
-const Img = styled.img`
-  position: fixed;
-  bottom: 0;
-  clip-path: polygon(
-    0% 20%,
-    100% 0%,
-    100% 100%,
-    100% 100%,
-    100% 100%,
-    100% 100%,
-    0% 100%
-  );
-  display: block;
-  max-height: 50vh;
-  width: auto;
-  height: auto;
-  opacity: 0.6;
-  @media (min-width: 630px) {
-    display: none;
-  }
-`;
-
-const LogIn = styled.p`
-  font-size: 1rem;
-  bottom: 3vh;
-`;
-
-const StyledLink = styled.a`
-  color: #0000EE;
-  text-decoration: underline;
-`;
-
-const Subtitle = styled.p`
-  margin: 1rem;
-`;
-
-const DesktopButton = styled(LinkButton)`
-  position: initial;
-  margin: 0.5rem;
-  @media (min-width: 630px) {
-    position: initial;
-    width: 90%;
-    height: 60px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-evenly;
-    align-items: center;
-    margin-bottom: 0.5rem;
-  }
-`;
-
-const H2 = styled.h2`
-  color: #7C53A2;
-  top: 3rem;
-`;
->>>>>>> parent of 18e0dbe... consistency throughout onboarding pages
+import {
+  Container,
+  Mask,
+  DesktopBg,
+  Card,
+  Img,
+  LogIn,
+  StyledLink,
+  Subtitle,
+  DesktopButton,
+  LinkButton,
+  H2
+} from "../styling/onboardingpages"
 
 export default class Landing extends Component {
   render() {
--- client/src/components/styling/onboardingpages.js
+++ client/src/components/styling/onboardingpages.js
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
 import LinkButton from "../LinkButton";
 
-
 const Container = styled.div`
   position: relative;
   font-size: 1rem;
+  background-image: url("data:image/svg+xml,%3Csvg width='52' height='26' viewBox='0 0 52 26' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%239C92AC' fill-opacity='0.4'%3E%3Cpath d='M10 10c0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6h2c0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4v2c-3.314 0-6-2.686-6-6 0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6zm25.464-1.95l8.486 8.486-1.414 1.414-8.486-8.486 1.414-1.414z' /%3E%3C/g%3E%3C/g%3E%3C/svg%3E"), linear-gradient(180deg, #7C53A2 0%, rgba(124, 83, 162, 0) 100%);
   @media (min-width: 768px) {
     position: relative;
     background-color: #7c53a2;
@@ -12,9 +12,10 @@ const Container = styled.div`
 `;
 
 const Mask = styled.div`
+  background-color: #7c53a2;
+  opacity: 0.5;
   @media (min-width: 768px) {
-    opacity: 0.5;
-    background-color: #7c53a2;
+
     width: 100vw;
     height: 100vh;
   }
