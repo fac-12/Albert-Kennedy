f4ee0b688d05835580219dfad8a7aa641958bbbd
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2018-05-02 18:12:05 +0100

    Refactor cancelAppointment promise chain

--- server/controllers/profileAppointments.js
+++ server/controllers/profileAppointments.js
@@ -1,6 +1,9 @@
 const airtable = require('../airtable/airtable_helpers');
-const { mentorCancellationEmail, userCancellationEmail } = require('../emails/sendCancellationEmails')
-const jwt = require('jwt-simple')
+const {
+  mentorCancellationEmail,
+  userCancellationEmail
+} = require('../emails/sendCancellationEmails');
+const jwt = require('jwt-simple');
 
 exports.profileAppointments = (req, res) => {
   airtable
@@ -18,38 +21,29 @@ exports.cancelAppointment = (req, res) => {
 
   airtable
     .getApptRecordId(appt.chat_string)
-    .then(async id => {
-      return await airtable.deleteAppointment(id)
-    })
+    .then(airtable.deleteAppointment)
     .then(() => airtable.getUserRecordId(userId))
-    .then(async userRecordId => {
-      return await airtable.getEmailDetails([ appt.mentor_id[0], userRecordId ])
-    })
+    .then(userRecordId =>
+      airtable.getEmailDetails([appt.mentor_id[0], userRecordId])
+    )
     .then(([mentorDetails, userDetails]) => {
-
-        mentorCancellationEmail(
-          { emailAddress: mentorDetails[0],
-            userName: userDetails[1],
-            date: appt.date_and_time
-          }
-          );
-        userCancellationEmail(
-          { emailAddress: userDetails[0],
-            userName: userDetails[1],
-            mentorName: appt.mentor_name,
-            date: appt.date_and_time 
-          }
-          );
-          return;
-      })
-    .then(async userId => {
-      return await airtable
-      .getUserAppointments(userId)
-      .then(airtable.addMentorDetailsToAppointments)
-      .then(appointments => {
-        res.send(JSON.stringify(appointments));
-        })
-      .catch(console.log)
-  })
-  .catch(console.log)
-}
+      mentorCancellationEmail({
+        emailAddress: mentorDetails[0],
+        userName: userDetails[1],
+        date: appt.date_and_time
+      });
+      userCancellationEmail({
+        emailAddress: userDetails[0],
+        userName: userDetails[1],
+        mentorName: appt.mentor_name,
+        date: appt.date_and_time
+      });
+      return;
+    })
+    .then(() => airtable.getUserAppointments(userId))
+    .then(airtable.addMentorDetailsToAppointments)
+    .then(appointments => {
+      res.send(JSON.stringify(appointments));
+    })
+    .catch(console.log);
+};
