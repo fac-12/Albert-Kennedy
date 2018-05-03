8f4910c9f22745b96764cea8f4ff6ebcb00ed980
Lucy King <lucy.m.king92@gmail.com>
2018-05-03 09:30:02 +0100

    Update update_password_email.js

--- server/emails/update_password_email.js
+++ server/emails/update_password_email.js
@@ -2,7 +2,9 @@
 const userEmailTemplate = user => {
     return `<p> Hi there ${user.name}, </p>
     <p>You've requested to change your password.</p>
-    <p>Please follow this link: ${user.passwordLink}</p>
+    <p>To do so, please follow this link: ${user.passwordLink}. Your token will expire in 24 hours.</p>
+    <p>Best Wishes,</p>
+    <p>The Albert Kennedy Trust</p>
     `;
   };
 
