9a4b2cb8584972a555cb1b0dcbdb0da4d8b15d40
Lucy King <lucy.m.king92@gmail.com>
2018-05-04 13:15:21 +0100

    update email reset:

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -114,7 +114,7 @@ exports.forgotPassword = (req, res) => {
         name: user.name,
         email: user.email,
         passwordLink:
-          "http://localhost:3000/resetpassword?token=" +
+          "http://inter-akt-staging.herokuapp.com/resetpassword?token=" +
           user.reset_password_token
       };
 
