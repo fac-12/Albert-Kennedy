8e53bbe9f0d1171156388f9b4dd1e5351db54a4e
lucymk <lucy.m.king92@gmail.com>
2018-11-26 13:48:24 +0000

    testing stuff with primitive node module

--- client/src/components/landing/LGBT.js
+++ client/src/components/landing/LGBT.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import primitive from "primitive";
 
 import {
   Container,
@@ -9,11 +10,43 @@ import {
   ButtonDiv
 } from "../styling/components";
 
+import plant from "../../assets/plant.jpg";
+
+class MyCanvas extends Component {
+  render() {
+    return (
+      <div>
+        <canvas ref="canvas" width={640} height={425} />
+      </div>
+    );
+  }
+}
+
+const opts = {
+  input: plant,
+  output: "canvas",
+  numSteps: 400,
+  numCandidateShapes: 20,
+  numCandidateMutations: 20
+};
+
+const newPic = primitive(opts);
+
+const showPic = () =>
+  newPic
+    .then(x => {
+      console.log(x, "hello");
+    })
+    .catch(console.log("error"));
+
+showPic();
+
 export default class LGBT extends Component {
   render() {
     return (
       <Container>
         <Card>
+          <MyCanvas />
           <H2>inter-AKT</H2>
           <Subtitle>Do you identify as part of the LGBT+ community?</Subtitle>
           <ButtonDiv>
